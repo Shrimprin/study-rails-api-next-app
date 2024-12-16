ac2cc79967f8990f024da66b20fd5d2daaf91c30
Kuroneko <ken.aster8@gmail.com>
2024-12-16 10:06:02 +0900

    ブラウザのデフォルトcssをリセット

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import * as React from "react";
+import "@/styles/destyle.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
