3d406547e3460a652787d60b64835d71ccd2fd14
Kuroneko <ken.aster8@gmail.com>
2024-12-16 10:38:03 +0900

    エラー画面をいい感じにした

---
+++ next/src/components/Error.tsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <div className="max-w-sm mx-auto mt-8 p-3 bg-gray-200">
+      <div className="leading-relaxed">
+        現在、システムに技術的な問題が発生しています。ご不便をおかけして申し訳ありませんが、復旧までしばらくお待ちください。
+      </div>
+    </div>
+  );
+};
+
+export default Error;
--- next/src/components/Loading.tsx
+++ next/src/components/Loading.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 const Loading = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
+      {/* イメージはこれ
+      https://loading.io/ */}
       <Image src="/loading.svg" width={150} height={150} alt="loading..." />
     </div>
   );
