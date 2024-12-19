0675205339b0c882b62d3df5c082dde0d9ae043b
Kuroneko <ken.aster8@gmail.com>
2024-12-20 07:59:31 +0900

    サインイン状態に応じてヘッダが変化するようにした

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useUserState } from "@/hooks/useGlobalState";
 
 const Header = () => {
+  const [user] = useUserState();
+
   return (
     <header className="bg-white text-black py-3 shadow-none">
       <div className="container mx-auto px-4">
@@ -11,16 +14,23 @@ const Header = () => {
               <Image src="/logo.png" width={133} height={40} alt="logo" />
             </Link>
           </div>
-          <div>
-            <Link href="/sign_in">
-              <button className="bg-blue-500 text-white text-lg rounded-md shadow-none px-4 py-2">
-                Sign in
-              </button>
-            </Link>
-            <button className="border border-blue-500 text-lg rounded-md shadow-none px-4 py-2 ml-2">
-              Sign Up
-            </button>
-          </div>
+          {user.isFetched && (
+            <>
+              {!user.isSignedIn && (
+                <div>
+                  <Link href="/sign_in">
+                    <button className="bg-blue-500 text-white text-lg rounded-md shadow-none px-4 py-2">
+                      Sign in
+                    </button>
+                  </Link>
+                  <button className="border border-blue-500 text-lg rounded-md shadow-none px-4 py-2 ml-2">
+                    Sign Up
+                  </button>
+                </div>
+              )}
+              {user.isSignedIn && <p>{user.name}</p>}
+            </>
+          )}
         </div>
       </div>
     </header>
